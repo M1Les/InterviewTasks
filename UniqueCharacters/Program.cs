8ea6dbe026b16c0124f25ce72ab75ed5a51770b4
George Guskov <m1lesnsk@gmail.com>
2016-11-17 20:44:31 +0700

    Added HasUnique

---
+++ UniqueCharacters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniqueCharacters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var str = "asdzxcver";
+
+            Console.WriteLine(HasUnique(str));
+            Console.ReadKey();
+        }
+
+        private static bool HasUnique(string str)
+        {
+            var dict = new Dictionary<char, bool>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (dict.ContainsKey(str[i]))
+                {
+                    return false;
+                }
+
+                dict.Add(str[i], true);
+            }
+
+            return true;
+        }
+    }
+}
