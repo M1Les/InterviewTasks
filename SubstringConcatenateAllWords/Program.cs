d448cb2a4ecbb41d636aa266ca050461621fff9a
George Guskov <m1lesnsk@gmail.com>
2016-11-18 21:29:25 +0700

    Add SubstringConcatenateAllWords
    You are given a string, s, and a list of words, words, that are all of
    the same length. Find all starting indices of substring(s) in s that is
    a concatenation of each word in words exactly once and without any
    intervening characters.
    
    For example, given:
    
    s: barfoothefoobarman
    
    words: [foo, bar]
    
    You should return the indices: [0,9].
    
    (order does not matter).

---
+++ SubstringConcatenateAllWords/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubstringConcatenateAllWords
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var res = FindSubstr(new string[] { "foo", "bar" }, "barfoothefoobarman");
+
+            foreach (var item in res)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static List<int> FindSubstr(string[] words, string s)
+        {
+            var res = new List<int>();
+
+            if (string.IsNullOrEmpty(s) || words.Length == 0)
+            {
+                return res;
+            }
+
+            var n = words.Length;
+            var k = s.Length;
+            var m = words[0].Length;
+
+            var needed = new Dictionary<string, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (needed.ContainsKey(words[i]))
+                {
+                    needed[words[i]]++;
+                }
+                else
+                {
+                    needed.Add(words[i], 1);
+                }
+            }
+
+            for (int i = 0; i <= k-n*m; i++)
+            {
+                int j = 0;
+                var actualLeft = new Dictionary<string, int>();
+                while (j < n)
+                {
+                    var word = s.Substring(i + j * m, m);
+                    if (needed.ContainsKey(word) == false)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (actualLeft.ContainsKey(word) == false)
+                        {
+                            actualLeft.Add(word, needed[word] - 1);
+                        }
+                        else
+                        {
+                            actualLeft[word]--;
+                        }
+                        if (actualLeft[word] < 0)
+                        {
+                            break;
+                        }
+                    }
+                    j++;
+                }
+                if (j == n)
+                {
+                    res.Add(i);
+                }
+            }
+
+            return res;
+        }
+    }
+}
