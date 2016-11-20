1d468afc75a6817f8a3733ba8a126a9e2297565c
George Guskov <m1lesnsk@gmail.com>
2016-11-20 21:46:20 +0700

    Add IsPrime

---
+++ IsPrime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsPrime
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsPrimeRecursive(7, 2));
+            Console.ReadKey();
+        }
+
+        public static bool IsPrimeRecursive(int x, int divisor)
+        {
+            if (x < 1)
+                return false;
+
+            if (x <= 3)
+                return true;
+
+            if (divisor > Math.Sqrt(x))
+                return true;
+            else if (x % divisor == 0)
+                return false;
+
+            return IsPrimeRecursive(x, divisor + 1);
+        }
+    }
+}
