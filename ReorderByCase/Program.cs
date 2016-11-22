95e5fc6f5b5baa90646e830f92e50981d1f9e4b0
George Guskov <m1lesnsk@gmail.com>
2016-11-22 21:26:58 +0700

    Add ReorderByCase inplace

--- ReorderByCase/Program.cs
+++ ReorderByCase/Program.cs
@@ -16,6 +16,14 @@ namespace ReorderByCase
             Console.WriteLine(OrderByCase(""));
             Console.WriteLine(OrderByCase("A"));
             Console.WriteLine(OrderByCase("z"));
+
+            Console.WriteLine(OrderByCaseInplace("sdfDSdfDSg".ToArray()));
+            Console.WriteLine(OrderByCaseInplace("QsdfDSdfDSg".ToArray()));
+            Console.WriteLine(OrderByCaseInplace("AsdfDSdfDSg".ToArray()));
+            Console.WriteLine(OrderByCaseInplace("".ToArray()));
+            Console.WriteLine(OrderByCaseInplace("A".ToArray()));
+            Console.WriteLine(OrderByCaseInplace("z".ToArray()));
+
             Console.ReadKey();
         }
 
@@ -47,6 +55,54 @@ namespace ReorderByCase
             }
 
             return first.ToString() + second.ToString();
+        }
+
+        public static string OrderByCaseInplace(char[] s)
+        {
+            if (s.Length == 0 || s.Length == 1)
+                return string.Concat(s);
+
+            int p1 = 1;
+            int p2 = s.Length - 1;
+
+            while (p1<s.Length && IsSameCase(s[p1], s[p1-1]))
+            {
+                p1++;
+            }
+
+            while (p1<p2)
+            {
+                if (IsDifferentCase(s[p1], s[p2]))
+                {
+                    var tmp = s[p1];
+                    s[p1] = s[p2];
+                    s[p2] = tmp;
+                    p1++;
+                    p2--;
+                }
+                else if (IsSameCase(s[p1], s[p2]) && IsSameCase(s[p1-1], s[p2]))
+                {
+                    p1++;
+                }
+                else
+                {
+                    p2--;
+                }
+            }
+
+            return string.Concat(s);
+        }
+
+        public static bool IsSameCase(char c1, char c2)
+        {
+            return (char.IsLower(c1) && char.IsLower(c2))
+                || (char.IsUpper(c1) && char.IsUpper(c2));
+        }
+
+        public static bool IsDifferentCase(char c1, char c2)
+        {
+            return (char.IsLower(c1) && char.IsUpper(c2))
+                || (char.IsUpper(c1) && char.IsLower(c2));
         }
     }
 }
