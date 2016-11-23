90d16fbc3e285d2b9552b1bb34e9464ad96e4e7c
George Guskov <m1lesnsk@gmail.com>
2016-11-23 20:51:44 +0700

    Add WordBreak
    Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
    
    For example, given
    
    s = leetcode,
    
    dict = [leet, code].
    
    Return true because leetcode can be segmented as leet code.

---
+++ WordBreak/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordBreak
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(WordBreak("123456123", new List<string> { "123", "456" }));
+            Console.ReadKey();
+        }
+
+        public static bool WordBreak(string s, IReadOnlyCollection<string> words)
+        {
+            bool[] matches = new bool[s.Length + 1];
+            matches[0] = true;
+
+            for (int end = 0; end < s.Length; end++)
+            {
+                for (int begin = 0; begin < s.Length; begin++)
+                {
+                    if (matches[begin] && words.Contains(s.Substring(begin, end-begin+1)))
+                    {
+                        matches[end + 1] = true;
+                        break;
+                    }
+                }
+            }
+
+            return matches[s.Length];
+        }
+    }
+}
