9d0e78b6919fbf86ca5fd7c312b9679b7f5db162
George Guskov <m1lesnsk@gmail.com>
2016-11-27 22:09:24 +0700

    Add TwoSum
    Given an array of integers, find two numbers such that they add up to a specific target number.
    
    The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
    
    You may assume that each input would have exactly one solution.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(SumTwo(new int[] { 2, 7, 11, 15}, 9));
+            Console.ReadKey();
+        }
+
+        public static Tuple<int, int> SumTwo(int[] num, int sum)
+        {
+            if (num.Length == 0)
+                return Tuple.Create(0, 0);
+
+            var results = new Dictionary<int, int>();
+            var n = num.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                int matchingIndex;
+
+                if (results.TryGetValue(sum - num[i], out matchingIndex))
+                {
+                    return Tuple.Create(matchingIndex + 1, i + 1);
+                }
+                else
+                {
+                    results.Add(num[i], i);
+                }
+            }
+
+            return Tuple.Create(0, 0);
+        }
+    }
+}
---
+++ TwoSum/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TwoSum")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("acf04512-266d-49a4-a0ed-3d7dbb52a15c")]
