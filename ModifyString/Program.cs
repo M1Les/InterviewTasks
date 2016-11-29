3ced6ae2b2873bea07688741a3d64d7799f0dfc7
George Guskov <m1lesnsk@gmail.com>
2016-11-29 20:29:20 +0700

    Add ModifyString
    Replace every space in the string with "%20"

---
+++ ModifyString/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModifyString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(ReplaceSpaces("123 3567 "));
+            Console.WriteLine(ReplaceSpaces(""));
+            Console.WriteLine(ReplaceSpaces(" "));
+            Console.WriteLine(ReplaceSpaces(" dfs"));
+            Console.WriteLine(ReplaceSpaces(" df  s   "));
+            Console.ReadKey();
+        }
+
+        public static string ReplaceSpaces(string str)
+        {
+            var newLength = str.Length;
+            for (int k = 0; k < str.Length; k++)
+            {
+                if (str[k] == ' ')
+                    newLength += 2;
+            }
+
+            var ret = new char[newLength];
+            var i = str.Length-1;
+            var j = newLength-1;
+
+            while (i >=0)
+            {
+                if (str[i] == ' ')
+                {
+                    ret[j] = '0';
+                    ret[j-1] = '2';
+                    ret[j-2] = '%';
+                    j -= 3;
+                }
+                else
+                {
+                    ret[j] = str[i];
+                    j--;
+                }
+
+                i--;
+            }
+            return new string(ret);
+        }
+    }
+}
---
+++ ModifyString/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ModifyString")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8269b2d1-5f2d-47e3-8fb4-3c70c2b1c26d")]
