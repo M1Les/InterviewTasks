e22571c541ade6d163930851b8beda99cb69a498
George Guskov <m1lesnsk@gmail.com>
2016-12-01 19:04:56 +0700

    Add ReorderParity
    One cycle implementation

--- ReorderParity/Program.cs
+++ ReorderParity/Program.cs
@@ -9,13 +9,39 @@ namespace ReorderParity
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(string.Join(",", Reorder(new int[] { 1,2,3,5,6,7})));
-            Console.WriteLine(string.Join(",", Reorder(new int[] { 2 })));
-            Console.WriteLine(string.Join(",", Reorder(new int[] { 2,2,2,2,2 })));
-            Console.WriteLine(string.Join(",", Reorder(new int[] { 3,3,3,3,3 })));
-            Console.WriteLine(string.Join(",", Reorder(new int[] { 3,3, 2, 2, 2 })));
-            Console.WriteLine(string.Join(",", Reorder(new int[] { 2,2, 3, 3, 3 })));
+            Console.WriteLine(string.Join(",", ReorderOneCycle(new int[] { 1,2,3,5,6,7})));
+            Console.WriteLine(string.Join(",", ReorderOneCycle(new int[] { 2 })));
+            Console.WriteLine(string.Join(",", ReorderOneCycle(new int[] { 2,2,2,2,2 })));
+            Console.WriteLine(string.Join(",", ReorderOneCycle(new int[] { 3,3,3,3,3 })));
+            Console.WriteLine(string.Join(",", ReorderOneCycle(new int[] { 3,3, 2, 2, 2 })));
+            Console.WriteLine(string.Join(",", ReorderOneCycle(new int[] { 2,2, 3, 3, 3 })));
             Console.ReadKey();
+        }
+
+        public static int[] ReorderOneCycle(int[] a)
+        {
+            if (a.Length == 0)
+                return a;
+
+            int i = 0;
+            int j = a.Length - 1;
+            while (i<j)
+            {
+                if (a[i] % 2 == 0)
+                    i++;
+                else if (a[j] % 2 != 0)
+                    j--;
+                else
+                {
+                    var tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                }
+            }
+
+            return a;
+
+            return a;
         }
 
         public static int[] Reorder(int[] a)
