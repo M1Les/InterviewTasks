ef228237b8078160114127f0c266072b19d8e909
George Guskov <m1lesnsk@gmail.com>
2016-12-04 16:55:11 +0700

    Added MinDifferenceInArray
    Given an unsorted array, find the minimum difference between any pair in given array.

---
+++ MinDifferenceInArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinDifferenceInArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(MinDiffBySorting(new int[] { 1, 2, 3 }));
+            Console.WriteLine(MinDiffBySorting(new int[] { 1, 5, 3, 19, 18, 25 }));
+            Console.WriteLine(MinDiffBySorting(new int[] { 30, 5, 20, 9 }));
+            Console.WriteLine(MinDiffBySorting(new int[] { 1, 19, -4, 31, 38, 25, 100 }));
+            Console.ReadKey();
+        }
+
+        public static int MinDiffBySorting(int[] a) // O(nlog(n))
+        {
+            if (a.Length == 0 || a.Length == 1)
+                return 0;
+            int[] sa = a.OrderBy(e => e).ToArray();
+            int minDiff = int.MaxValue;
+            for (int i = 1; i < sa.Length; i++)
+            {
+                 minDiff = Math.Min(minDiff, sa[i] - sa[i - 1]); // no need for modulo since i >= i-1 because we sorted the array
+            }
+
+            return minDiff;
+        }
+    }
+}
