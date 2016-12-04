8c964a329df19a125e3075918a6d1efbc11568d5
George Guskov <m1lesnsk@gmail.com>
2016-12-04 16:56:10 +0700

    Added MinDifferenceTwoArray
    Find min defference between any pair from two given arrays.

---
+++ MinDifferenceTwoArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinDifferenceTwoArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(MinDiff(new int[] { 1,5,7 }, new int[] { 2, 3, 5, 6}));
+            Console.ReadKey();
+        }
+
+        public static int MinDiff(int[] a, int[] b)
+        {
+            if (a.Length == 0 || b.Length == 0)
+                return 0;
+
+            var sa = a.OrderBy(e => e).ToArray();
+            var sb = b.OrderBy(e => e).ToArray();
+            int i = 0;
+            int j = 0;
+            int min = int.MaxValue;
+
+            while (i< sa.Length && j < sb.Length)
+            {
+                if (Math.Abs(sa[i] - sb[j]) < min)
+                    min = Math.Abs(sa[i] - sb[j]);
+
+                if (sa[i] < sb[j])
+                    i++;
+                else if (sa[i] > sb[j])
+                    j++;
+                else
+                {
+                    i++;
+                    j++;
+                }
+            }
+
+            return min;
+        }
+    }
+}
