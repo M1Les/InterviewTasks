f000ffd224eca745a89504ab7ff9d8d03ce76f7c
George Guskov <m1lesnsk@gmail.com>
2016-12-07 21:17:07 +0700

    Added LongestWordsInDictionary

---
+++ LongestWordsInDictionary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LongestWordsInDictionary
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", FindLongest(new string[] { "1", "1", "1", "1", "1", "1", "" })));
+            Console.WriteLine(string.Join(",", FindLongest(new string[] { "1", "1", "1", "1", "1", "1" })));
+            Console.WriteLine(string.Join(",", FindLongest(new string[] { "", "1", "1", "1", "1", "1" })));
+            Console.WriteLine(string.Join(",", FindLongest(new string[] { "", "1", "1", "1", "", "1" })));
+            Console.WriteLine(string.Join(",", FindLongest(new string[] { "1", "1", "11223", "11", "1", "12", "" })));
+            Console.WriteLine(string.Join(",", FindLongest(new string[] { "1", "1", "11223", "11", "1", "12", "11223" })));
+            Console.WriteLine(string.Join(",", FindLongest(new string[] { "1", "1", "11223", "11", "1", "12", "11223" })));
+            Console.WriteLine(string.Join(",", FindLongest(new string[] { "" })));
+            Console.WriteLine(string.Join(",", FindLongest(new string[] { "","" })));
+            Console.ReadKey();
+        }
+
+        public static ICollection<string> FindLongest(string[] a)
+        {
+            var n = a.Length;
+
+            var ret = new List<string>();
+            int maxLength = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i].Length > maxLength)
+                {
+                    ret = new List<string>();
+                    maxLength = a[i].Length;
+                }
+                if (maxLength == a[i].Length)
+                {
+                    ret.Add(a[i]);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
