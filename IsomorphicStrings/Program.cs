fa256fd9be52255fed847a43f40df4641f7b7053
George Guskov <m1lesnsk@gmail.com>
2016-12-13 20:57:40 +0700

    Added IsomorphicStrings
    Given two strings s and t, determine if they are isomorphic.
    
    Two strings are isomorphic if the characters in s can be replaced to get t.
    
    All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
    
    For example,
    
    Given "egg", "add", return true.
    Given "foo", "bar", return false.
    Given "paper", "title", return true.
    Note:
    
    You may assume both s and t have the same length.

---
+++ IsomorphicStrings/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsomorphicStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsIso("abb","edd"));
+            Console.WriteLine(IsIso("abb", "edf"));
+            Console.ReadKey();
+        }
+
+        public static bool IsIso(string a, string b)
+        {
+            if (a.Length == 0 && b.Length == 0)
+                return true;
+
+            if (a.Length != b.Length)
+                return false;
+
+            var map = new Dictionary<char, char>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (!map.ContainsKey(a[i]))
+                {
+                    map.Add(a[i], b[i]);
+                }
+                else if (map[a[i]] != b[i])
+                {
+                    return false;
+                }
+            }
+            map.Clear();
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (!map.ContainsKey(b[i]))
+                {
+                    map.Add(b[i], a[i]);
+                }
+                else if (map[b[i]] != a[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
